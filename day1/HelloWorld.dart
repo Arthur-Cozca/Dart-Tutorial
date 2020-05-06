19d00c644e15acb45f2dfb79592d34daa48ab4ea
Arthur Servantie <arthur.servantie@protonmail.com>
2020-05-06 16:14:49 +0200

    Day 1

---
+++ day1/HelloWorld.dart
@@ -0,0 +1,19 @@
+// Let's manipulate some variable
+// For this first day, we will learn how to manipulate variables...
+
+/* Let's begin! To create a variable in the Dart language, there are 2 ways: 
+use the keyword "var" followed by the name of the variable and then assign its value, 
+or use the type of the variable, then its name and then its value.*/
+
+/*There are several types of values whether in the Dart or in other languages. 
+There is the String type, which we will use for strings, 
+the int type for integers, 
+the double type for numbers with an infinity of digits after the decimal point, 
+and the float type, numbers with a maximum of 2 digits after the decimal point.*/ 
+
+/*Let's create our first helloWorld variable which will therefore have the following string type "HelloWorld! as you can see, 
+this variable will have a string type. We will create it in both ways.*/
+void main(){
+  var helloWorld = "Hello World !";
+  String HelloWorld = "HelloWorld";
+}
