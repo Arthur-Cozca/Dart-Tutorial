163a39b2bbaf475544d82cbd3b6a517c8e9414e5
Arthur Servantie <arthur.servantie@protonmail.com>
2020-05-07 10:48:47 +0200

    FR Version

--- day1/HelloWorld-FR.dart
+++ day1/HelloWorld-FR.dart
@@ -14,4 +14,13 @@ et le type float, les nombres avec un maximum de 2 chiffres après la virgule.*/
 /*Créons notre première variable helloWorld qui aura donc la chaîne suivante "HelloWorld ! comme vous pouvez le voir, 
 cette variable aura un type String. Nous la créerons dans les deux sens.*/
 
+/*Maintenant, jetons un coup d'œil à ce que nous venons d'écrire. Vous pouvez voir que nous avons commencé notre programme avec la ligne suivante : void main() {}, 
+mais quelle est cette ligne ? Cette ligne est la fonction principale de notre programme, sans elle, le programme ne fonctionnerait pas, c'est comme le pain, sans eau, il ne fonctionne pas.*/
+/*Tout le code de notre programme devra être dans cette fonction, sinon, ce sera le vide */
+
+// Dans le prochain cours, nous verrons comment fonctionnent les conditions avec Dart. J'espère que ce cours sur les variables de Dart vous en apprendra davantage ! A la semaine prochaine
+// ps : sans le savoir, vous savez maintenant comment faire des commentaires dans Dart ! C'est amusant, n'est-ce pas ? 
+// Regardez l'exercice sur le fichier TP.md
+
+
 
