5151137f8212d349d985c284bf7c5b4f443bb0f6
Arthur Servantie <arthur.servantie@protonmail.com>
2020-05-07 10:46:18 +0200

    FR Version

--- day1/HelloWorld-FR.dart
+++ day1/HelloWorld-FR.dart
@@ -1,1 +1,17 @@
+// Manipulons une variable
+// Pour cette première journée, nous apprendrons à manipuler les variables...
+
+/* Commençons ! Pour créer une variable dans le langage Dart, il y a 2 façons : 
+utiliser le mot-clé "var" suivi du nom de la variable, puis lui attribuer une valeur, 
+ou utiliser le type de la variable, puis son nom et enfin sa valeur.*/
+
+/*Il existe plusieurs types de valeurs, que ce soit dans le Dart ou dans d'autres langues. 
+Il y a le type String, que nous utiliserons pour les chaînes de caractères, 
+le type int pour les entiers, 
+le type double pour les nombres avec une infinité de chiffres après la virgule, 
+et le type float, les nombres avec un maximum de 2 chiffres après la virgule.*/ 
+
+/*Créons notre première variable helloWorld qui aura donc la chaîne suivante "HelloWorld ! comme vous pouvez le voir, 
+cette variable aura un type String. Nous la créerons dans les deux sens.*/
+
 
